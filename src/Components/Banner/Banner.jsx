fb726a12d77979583291f5e8aa7cdbd4ab846edb
Abhinav <abhinavkinarullathil@gmail.com>
2024-01-27 12:33:06 +0530

    changes applied

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./Banner.css";
-import { FaXTwitter, FaWhatsapp, FaYoutube, FaFacebook } from "react-icons/fa6";
+
 
 export default function Banner() {
   const [show, setShow] = useState(1);
@@ -99,13 +99,7 @@ export default function Banner() {
             } banner_image image-animation`}
           />
         </>
-        {/* Side Social media details */}
-        <div className="h-28 md:h-40 w-5 md:w-10 fixed z-90 bg-yellow-500 bg-opacity-50 hover:bg-opacity-90 rounded-tr-lg rounded-br-lg mt-11 md:mt-44 md:text-2xl text-sm flex flex-col justify-center gap-2 items-center m-0 p-0">
-          <FaXTwitter className="hover:text-white  group/twit cursor-pointer" />
-          <FaWhatsapp className="hover:text-green-500 group/what cursor-pointer" />
-          <FaYoutube className="hover:text-red-600 group/you cursor-pointer" />
-          <FaFacebook className="hover:text-blue-500 group/face cursor-pointer" />
-        </div>
+     
       </div>
     </>
   );
