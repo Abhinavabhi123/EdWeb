1d66811b6a10f80426d63cd1af6c52223c4d7cf8
Abhinav <abhinavkinarullathil@gmail.com>
2024-02-06 20:04:30 +0530

    chnages applied in the academic section

--- src/Components/Academic/Academics.jsx
+++ src/Components/Academic/Academics.jsx
@@ -57,19 +57,19 @@ export default function Academics() {
 
 
   return (
-    <div className="w-full min-h-[40rem] academic_card py-5 px-10">
+    <div className="w-full min-h-[40rem] academic_card py-5 md:mb-20 mb-10 px-10">
       <div className="flex justify-center">
         <h1 className="text-2xl underline font-semibold">Academic Results</h1>
       </div>
       {/*  */}
       <div className="flex justify-center items-center gap-2 py-4">
-        <h2 className="font-semibold">Grade 10 Results (2023-2024)</h2>
-        <img src="/crown.png" alt="crown image" className="w-14" />
+        <h2 className="font-semibold ">Grade 10 Results (2023-2024)</h2>
+        <img src="/crown.png" alt="crown image" className="w-[2rem] md:w-14" />
       </div>
       {/* topper div */}
       <div>
         <div className="flex justify-start md:ps-40 py-4">
-          <h3 className="font-semibold">Overall Pass Percentage: 98%</h3>
+          <h3 className="font-semibold text-sm md:text-base" >Overall Pass Percentage: 98%</h3>
         </div>
         <div className="flex flex-wrap h-fit justify-center items-center gap-0 max-[768px]:gap-10 md:gap-20  md:p-10">
           {data?.map((item, i) => {
@@ -129,7 +129,7 @@ export default function Academics() {
       <div className="w-full flex flex-col mt-5">
         <div className="flex justify-center items-center gap-2 py-4">
         <h2 className="font-semibold">Grade 12 Results (2023-2024)</h2>
-        <img src="/crown.png" alt="crown image" className="w-14" />
+        <img src="/crown.png" alt="crown image" className="w-[2rem] md:w-14" />
       </div>
         <div className="w-full bg-transparent flex justify-center items-center">
           <div className="max-[768px]:w-[85%] w-full bg-transparent max-[496px]:p-14 flex items-center justify-center flex-col md:flex-row flex-wrap md:gap-20">
