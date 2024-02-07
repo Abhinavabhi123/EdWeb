36c19d04c857a2f31a21d0449ed721beb02b93a9
Abhinav <abhinavkinarullathil@gmail.com>
2024-02-07 11:14:05 +0530

    changed color of the card in academic result

--- src/Components/Academic/Academics.jsx
+++ src/Components/Academic/Academics.jsx
@@ -57,33 +57,33 @@ export default function Academics() {
 
 
   return (
-    <div className="w-full min-h-[40rem] academic_card py-5 md:mb-20 mb-10 px-10">
+    <div className="w-full min-h-[40rem] bg-slate-200 py-5 md:mb-20 mb-10 md:px-10">
       <div className="flex justify-center">
         <h1 className="text-2xl underline font-semibold">Academic Results</h1>
       </div>
       {/*  */}
-      <div className="flex justify-center items-center gap-2 py-4">
+      <div className="flex justify-center items-center gap-2 ">
         <h2 className="font-semibold ">Grade 10 Results (2023-2024)</h2>
         <img src="/crown.png" alt="crown image" className="w-[2rem] md:w-14" />
       </div>
       {/* topper div */}
       <div>
-        <div className="flex justify-start md:ps-40 py-4">
+        <div className="flex md:justify-start justify-center md:ps-40 py-4">
           <h3 className="font-semibold text-sm md:text-base" >Overall Pass Percentage: 98%</h3>
         </div>
-        <div className="flex flex-wrap h-fit justify-center items-center gap-0 max-[768px]:gap-10 md:gap-20  md:p-10">
+        <div className="flex flex-wrap  h-fit justify-center items-center gap-0 max-[768px]:gap-10 md:gap-20  ">
           {data?.map((item, i) => {
             return (
               <div
                 key={i}
-                className="w-[15rem] h-[18rem] bg-transparent flex justify-center flex-col items-center gap-3"
+                className="w-[15rem] h-[15rem] bg-transparent  flex justify-center  flex-col items-center gap-3"
                 data-aos="zoom-in"
               >
                 <div className="w-40 h-40 rounded-full">
                   <img
                     src={item?.img}
                     alt="image"
-                    className=" rounded-full object-cover w-full h-full"
+                    className=" rounded-full object-cover w-full h-full  hover:scale-90 transition-all duration-300 cursor-pointe"
                   />
                 </div>
 
@@ -96,11 +96,11 @@ export default function Academics() {
       {/*  */}
 
       <div className="flex flex-col">
-        <div className="flex justify-start md:ps-40 py-4">
+        <div className="flex md:justify-start justify-center md:ps-40 py-4">
           <h3 className="font-semibold">Subject-wise Performance:</h3>
         </div>
         <div className="flex justify-center items-center">
-          <div className="max-[768px]:w-[85%] w-full bg-transparent max-[496px]:p-14 flex items-center justify-center flex-col md:flex-row flex-wrap gap-8">
+          <div className="max-[768px]:w-[85%] w-full bg-transparent max-[496px]:p-14 flex items-center  justify-center flex-col md:flex-row gap-8">
             {subjectWise.map((item, i) => {
               return (
                 <div
@@ -109,12 +109,12 @@ export default function Academics() {
                   data-aos="zoom-in"
                 >
                   <p className="text-center w-full md:h-10">{item?.subject}</p>
-                  <div className="w-full h-[17rem] bg-slate-200 rounded-lg flex flex-col justify-center gap-3 items-center">
+                  <div className="w-full h-[17rem] bg-slate-300 rounded-lg flex flex-col justify-center gap-3 items-center">
                     <div className="w-40 h-40 rounded-full">
                       <img
                         src={item?.img}
                         alt="image"
-                        className="rounded-full w-full h-full"
+                        className="rounded-full w-full h-full hover:scale-90 transition-all duration-300 cursor-pointer"
                       />
                     </div>
                     <p className="w-full text-center">{item?.name}</p>
@@ -127,26 +127,26 @@ export default function Academics() {
       </div>
       {/*  */}
       <div className="w-full flex flex-col mt-5">
-        <div className="flex justify-center items-center gap-2 py-4">
+        <div className="flex justify-center items-center gap-2 ">
         <h2 className="font-semibold">Grade 12 Results (2023-2024)</h2>
         <img src="/crown.png" alt="crown image" className="w-[2rem] md:w-14" />
       </div>
-        <div className="w-full bg-transparent flex justify-center items-center">
-          <div className="max-[768px]:w-[85%] w-full bg-transparent max-[496px]:p-14 flex items-center justify-center flex-col md:flex-row flex-wrap md:gap-20">
+        <div className="w-full flex justify-center items-center">
+          <div className="max-[768px]:w-[85%] w-full bg-transparent flex items-center justify-center flex-col md:flex-row flex-wrap md:gap-4 ">
             {plusTwoData.map((item, i) => {
               return (
                 <div
                   key={i}
-                  className="w-[15rem] h-[18rem] bg-transparent flex justify-center flex-col  items-center md:gap-3"
+                  className="w-[15rem] h-[18rem] bg-transparent flex justify-center flex-col  items-center md:gap-3 "
                   data-aos="zoom-in"  
                 >
                   <p className="text-center w-full md:h-10">{item?.subject}</p>
-                  <div className="w-full h-[17rem] bg-slate-200 rounded-lg flex flex-col justify-center gap-3 items-center">
+                  <div className="w-full h-[17rem] bg-slate-300 rounded-lg flex flex-col justify-center gap-3 items-center">
                     <div className="w-40 h-40 rounded-full">
                       <img
                         src={item?.img}
                         alt="image"
-                        className="rounded-full w-full h-full"
+                        className="rounded-full w-full h-full hover:scale-90 transition-all duration-300 cursor-pointer"
                       />
                     </div>
                     <p className="w-full text-center">{item?.name}</p>
@@ -159,7 +159,7 @@ export default function Academics() {
       </div>
 
       <div className="w-full flex justify-center items-center py-4 mt-5">
-        <button className="px-5 py-3 font-semibold bg-[#c89734] rounded-lg group/btn"><a className="group-hover/btn:underline transition-all duration-500">Know More</a></button>
+        <button className="px-5 py-3 font-semibold bg-[#ddac4b] rounded-lg group/btn"><a className="group-hover/btn:underline transition-all duration-500">Know More</a></button>
       </div>
     </div>
   );
